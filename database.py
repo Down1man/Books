c40365bee3333c697e1dab1208f30e45a0204659
unknown <olszewski434@interia.pl>
2022-06-13 13:59:54 +0200

    make it better

--- database.py
+++ database.py
@@ -3,7 +3,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 #engine = create_engine("postgresql://postgres:Lijopleurodon12#4@localhost/database", echo=True)
-engine = create_engine("postgresql://wppnfwageosvfn:10f68c34e79be33239046277597ae2fe9ad9abbb3512617cfe3fdd457ddc94a1@ec2-52-72-99-110.compute-1.amazonaws.com:5432/d8md2fjpejrtsr")
+engine = create_engine("postgresql://erjdqsphgnfdfv:34efc68fe9d48c3925e73abc1e90851e7b7bae6a1073f2a6c5adb71ab928c929@ec2-52-72-99-110.compute-1.amazonaws.com:5432/d394s9u0corcpe")
 
 Base = declarative_base()
 
